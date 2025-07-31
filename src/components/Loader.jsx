9cd23ff7feac4e7a864def8078f268adc3915630
Rahaf Ziadeh <rahaf1234.ziadeh@gmail.com>
2025-07-31 10:21:40 +0300

    feat: add photo selection and loader component - Users can select photos and see the selected count - Added Loader component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../App.css";
+
+const Loader = () => {
+  return <div>Loading ..</div>;
+};
+
+export default Loader;
--- src/screens/AlbumScreen.jsx
+++ src/screens/AlbumScreen.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { fetchPhotos } from "../services/PhotoFetch";
 import Card from "../components/card";
+import Loader from "../components/Loader";
 
 function AlbumScreen() {
   const [photos, setPhotos] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [selected, setSelected] = useState([]);
 
   useEffect(() => {
     async function loadPhotos() {
@@ -22,19 +24,35 @@ function AlbumScreen() {
     loadPhotos();
   }, []);
 
-  if (loading) return <p>Loading photos...</p>;
+  const handleSelect = (id) => {
+  setSelected((prevSelected) => {
+    const alreadySelected = prevSelected.includes(id);
+
+    if (alreadySelected) {
+      return prevSelected.filter((item) => item !== id);
+    } else {
+      return [...prevSelected, id];
+    }
+  });
+};
+
+  if (loading) return <Loader />;
   if (error) return <p>Error: {error}</p>;
 
   return (
     <div className="app-container">
-      <h2 className="photo-heading">Fetched {photos.length} photos</h2>
-
+      <h2 className="photo-heading">
+        Fetched {photos.length} photos | Selected: {selected.length}
+      </h2>
       <div className="photos-grid">
         {photos.map((photo) => (
           <Card
             key={photo.id}
-            thumbnail={photo.thumbnailUrl}
+            id={photo.id}
+            thumbnailUrl={photo.thumbnailUrl}
             alt={photo.title}
+            isSelected={selected.includes(photo.id)}
+            onClick={handleSelect}
           />
         ))}
       </div>
